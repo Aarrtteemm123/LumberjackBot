c68da3f01fb22baa18ba9025d840acdba00af3f3
Artem <artem@gmail.com>
2020-01-17 15:25:35 +0200

    algorithm optimization

--- main.py
+++ main.py
@@ -3,44 +3,52 @@ import time
 from PIL import Image
 from ctypes import *
 
+
+def search_pixel(pixels, depth, x_vertical):
+    for y in range(depth):
+        if pixels[x_vertical, y] == (161, 116, 56) or pixels[40, y] == (136, 99, 50):
+            return True
+    return False
+
+
 time.sleep(2)
-adder = CDLL('butFunctions.dll')
-adder.clickLeftBut()
+clicker = CDLL('butFunctions.dll')
+clicker.clickLeftBut()
 last_key = 'left'
-# wood - 161,116,56
-# 60 fps - 0,0166666666666667 sec/frame
-pause = 0.1  # 0.1
-sleep = 0.04  # 0.07
+pause = 0.03  # 0.1
+sleep = 0.00  # 0.07
 i = 0
 while True:
     print('Start new loop')
     start = time.time()
 
-    screen = pyautogui.screenshot('frame\screen' + str(i) + '.png', region=(840, 360, 260, 100))
+    left = pyautogui.screenshot('left_frames\screen' + str(i) + '.png', region=(840, 220, 80, 220))
+    right = pyautogui.screenshot('right_frames\screen' + str(i) + '.png', region=(1000, 220, 80, 220))
     # print('Screenshot: '+str(time.time()-start))
 
     # start = time.time()
 
-    pixels = Image.open('frame\screen' + str(i) + '.png').load()
+    left_pixels = Image.open('left_frames\screen' + str(i) + '.png').load()
+    right_pixels = Image.open('right_frames\screen' + str(i) + '.png').load()
 
     # print('Convert: ' + str(time.time() - start))
     # start = time.time()
 
-    if pixels[0, 0] == (126, 173, 79):
-        adder.clickRightBut()
+    if search_pixel(left_pixels, 200, 40):
+        clicker.clickRightBut()
         time.sleep(pause)
         last_key = 'right'
-    if pixels[259, 0] == (126, 173, 79):
-        adder.clickLeftBut()
+    if search_pixel(right_pixels, 200, 40):
+        clicker.clickLeftBut()
         time.sleep(pause)
         last_key = 'left'
     if last_key == 'right':
-        adder.clickRightBut()
+        clicker.clickRightBut()
         time.sleep(pause)
     if last_key == 'left':
-        adder.clickLeftBut()
+        clicker.clickLeftBut()
         time.sleep(pause)
 
     time.sleep(sleep)
-    i += 1
+    i = 1
     print('Processing: ' + str(time.time() - start))
